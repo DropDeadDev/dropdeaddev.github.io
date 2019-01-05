c93e6698361b7c3946d87cd49aaea7739e1d40ac
Oleg Panasenko <32162077+DropDeadDev@users.noreply.github.com>
2019-01-05 16:17:13 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -2,4 +2,10 @@ $(function() {
 
 	// Custom JS
 
+	$('.top-line').after('<div class="menu__mobile d-lg-none"></div>');
+	$('.menu__top').clone().appendTo('.menu__mobile');
+	$('.menu__button').click(function() {
+		$('.menu__mobile').stop().slideToggle();
+	});
+
 });
